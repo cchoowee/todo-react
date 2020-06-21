d38828a7949db175b02989cbf9ca5db748ef0225
Carl Hughey <chughey@carlbasement.home.tld>
2020-06-21 15:32:27 -0400

    Updates

---
+++ precache-manifest.0aac0508dae6f340ae1886ac94419a93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fbd9649ae02d28604bae3ad888275b1",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "ec3eb67893f84fd6fd10",
+    "url": "/todo-react/static/css/main.2efa2c39.chunk.css"
+  },
+  {
+    "revision": "e1c993e3070ff6d419bc",
+    "url": "/todo-react/static/js/2.aadc151e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.aadc151e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec3eb67893f84fd6fd10",
+    "url": "/todo-react/static/js/main.013d7b25.chunk.js"
+  },
+  {
+    "revision": "7607a1ad8d38811108ba",
+    "url": "/todo-react/static/js/runtime-main.5d52d00f.js"
+  }
+]);
